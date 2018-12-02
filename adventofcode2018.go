95a45ce2107d2e4c283c5d20e6ee8dfe0b513546
piotrplaneta <piotr.planeta@gmail.com>
2018-12-02 10:46:10 +0100

    Solve day2

--- adventofcode2018.go
+++ adventofcode2018.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/piotrplaneta/adventofcode2018/day1"
+	"github.com/piotrplaneta/adventofcode2018/day2"
 )
 
 func main() {
 	fmt.Println(day1.SolvePart1())
 	fmt.Println(day1.SolvePart2())
+	fmt.Println(day2.SolvePart1())
+	fmt.Println(day2.SolvePart2())
 }
